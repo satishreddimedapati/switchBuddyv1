3d41565f7a4ba95bca74513bf8a71fb41ee6cc77
Satish Reddy <medapatisatishreddy2024@gmail.com>
2025-08-17 05:18:23 +0000

    accordian will be closed insitally on click it will opena cc..

--- src/app/daily-tracker/DailySchedule.tsx
+++ src/app/daily-tracker/DailySchedule.tsx
@@ -96,7 +96,7 @@ export function DailySchedule() {
           <Skeleton className="h-24 w-full" />
         </div>
       ) : (
-        <Accordion type="multiple" defaultValue={['Morning', 'Afternoon', 'Evening']} className="w-full space-y-4">
+        <Accordion type="multiple" className="w-full space-y-4">
             {Object.entries({ Morning: morning, Afternoon: afternoon, Evening: evening }).map(([period, periodTasks]) => 
                 periodTasks.length > 0 && (
                     <Card key={period}>
